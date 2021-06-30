59d3ea0c068bf145ed02462df93e844b1eb75bf4
Adrian Guery <adrianguery@gmail.com>
2021-06-30 11:29:49 +0200

    Implementation of a firestore counter with FieldValue.increment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useState} from 'react'
+
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+if (!firebase.apps.length) {
+  firebase.initializeApp({
+    //Config Firebase
+  });
+}else {
+  firebase.app(); // if already initialized, use that one
+}
+
+const firestore = firebase.firestore();
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Counter test</h1>
+        <Counters/>
       </header>
     </div>
   );
+}
+
+function Counters(){
+const [toolpadDoc, setToolpadDoc] = useState()
+
+
+  useEffect(() => {
+    fetData();
+  }, [])
+
+  const fetData=async()=>{
+    firestore.collection('events').doc('toolpad-salon').onSnapshot((doc)=>{
+      setToolpadDoc(doc.data());
+    })
+  }
+  
+  const increment= () => {
+    const increment = firebase.firestore.FieldValue.increment(1);
+    const toolpadDocRef = firestore.collection('events').doc('toolpad-salon')
+    toolpadDocRef.update({ numPhysicalParticipantIn: increment });
+  }
+
+  const increment100= () => {
+    const increment = firebase.firestore.FieldValue.increment(1);
+    const toolpadDocRef = firestore.collection('events').doc('toolpad-salon')
+    for(let i = 0; i<100;i++){
+      toolpadDocRef.update({ numPhysicalParticipantIn: increment });
+    }
+  }
+
+  const increment10= () => {
+    const increment10 = firebase.firestore.FieldValue.increment(10);
+    const toolpadDocRef = firestore.collection('events').doc('toolpad-salon')
+    toolpadDocRef.update({ numPhysicalParticipantIn: increment10 });
+  }
+
+  const decrement= () => {
+    const decrement = firebase.firestore.FieldValue.increment(1);
+    const toolpadDocRef = firestore.collection('events').doc('toolpad-salon')
+    toolpadDocRef.update({ numPhysicalParticipantOut: decrement });
+  }
+
+  const decrement10= () => {
+    const decrement10 = firebase.firestore.FieldValue.increment(10);
+    const toolpadDocRef = firestore.collection('events').doc('toolpad-salon')
+    toolpadDocRef.update({ numPhysicalParticipantOut: decrement10 });
+  }
+
+  return(
+    <div>
+      <h2>In : {toolpadDoc ? toolpadDoc.numPhysicalParticipantIn : null}</h2>
+      <h2>Out : {toolpadDoc ? toolpadDoc.numPhysicalParticipantOut : null}</h2>
+      <div>
+        <button onClick={()=>{increment()}}>+1</button>
+        <button onClick={()=>{decrement()}}>-1</button>
+        <button onClick={()=>{increment10()}}>+10</button>
+        <button onClick={()=>{decrement10()}}>-10</button>
+      </div>
+        <button onClick={()=>{increment100()}}>+100</button>
+    </div>
+  )
 }
 
 export default App;
